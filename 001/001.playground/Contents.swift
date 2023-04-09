be9736719a9b0c94cd1f21ebc2c39e0b9f05183b
d5f6 <dennisfreitag@posteo.de>
2023-04-09 12:40:46 +0200

    day 1

---
+++ 001/001.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+
